f0438392bd2306876c4d9a4a9589077cecbb17a7
petherin <petherin@gmail.com>
2016-02-05 13:30:51 +0000

    InitialCheckin

---
+++ SkyBill/Models/Bill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkyBill.Models
+{
+    public class Bill
+    {
+        public Statement statement { get; set; }
+        public double total { get; set; }
+        public Package package { get; set; }
+        public CallCharges callCharges { get; set; }
+        public SkyStore skyStore { get; set; }
+    }
+}
---
+++ SkyBill/Models/BuyAndKeep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkyBill.Models
+{
+    public class BuyAndKeep
+    {
+        public string title { get; set; }
+        public double cost { get; set; }
+    }
+}
